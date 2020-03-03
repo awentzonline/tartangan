f5e6d9804500fc237d37f1a9cb8a0f98374a813c
Adam Wentz <adam@vody.com>
2020-03-03 14:03:53 -0800

    Fix for SelfAttention2d

--- tartangan/models/blocks.py
+++ tartangan/models/blocks.py
@@ -236,6 +236,9 @@ class IQNDiscriminatorOutput(nn.Module):
 
 
 class SelfAttention2d(nn.Module):
+    """
+    Follows https://github.com/heykeetae/Self-Attention-GAN/blob/master/sagan_models.py
+    """
     def __init__(self, in_dims, attention_dims=None):
         super().__init__()
         if attention_dims is None:
@@ -264,7 +267,7 @@ class SelfAttention2d(nn.Module):
         v = v.view(batch, channels, -1)
         q = q.permute((0, 2, 1))
         attention = torch.matmul(q, k)
-        attention = F.softmax(attention, dim=1)
-        attended = torch.matmul(v, attention)
+        attention = F.softmax(attention, dim=-1)
+        attended = torch.matmul(v, attention.permute(0, 2, 1))
         attended = attended.view(batch, channels, height, width)
         return self.gamma * attended + x
