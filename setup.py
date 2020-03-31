ec7cf240a40bdc598a8cfa832046e72c5a3f71aa
Adam Wentz <adam@adamwentz.com>
2020-03-31 14:20:31 -0700

    Bump version 0.2.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name="tartangan",
-    version="0.1.0",
+    version="0.2.0",
     author="Adam Wentz",
     author_email="adam@adamwentz.com",
     description="Model tartan patterns with a GAN.",
