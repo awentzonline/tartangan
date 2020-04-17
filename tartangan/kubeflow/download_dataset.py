0e763163c183b2d30a9bb72f1138ee345fb290ed
Adam Wentz <adam@vody.com>
2020-04-17 09:39:39 -0700

    Work on integrating kubeflow metadata--dataset registration

---
+++ tartangan/kubeflow/download_dataset.py
@@ -0,0 +1,40 @@
+import os
+
+from kubeflow.metadata import metadata
+import smart_open
+
+from .base_metadata_app import BaseMetadataApp
+
+
+class DownloadDatasetMetadata(BaseMetadataApp):
+    def run(self):
+        super().run()
+        datasets = self.find_datasets_by_name(self.args.dataset_name)
+        # TODO: need to sort to get latest?
+        dataset = datasets[-1]
+        with smart_open.open(dataset['uri'], 'rb') as infile:
+            with smart_open.open(self.args.output_path, 'wb') as outfile:
+                outfile.write(infile.read())
+
+    def find_datasets_by_name(self, name):
+        artifacts = self.workspace.list(metadata.DataSet.ARTIFACT_TYPE_NAME)
+        print(artifacts)
+        matches = [
+            artifact for artifact in artifacts
+            if artifact['name'] == name
+        ]
+        return matches
+
+    @classmethod
+    def add_args_to_parser(cls, p):
+        super().add_args_to_parser(p)
+        p.add_argument('dataset_name', help='Name of metadata entity')
+        p.add_argument('output_path', help='Where the corresponding files go')
+
+
+def main():
+    DownloadDatasetMetadata.run_from_cli()
+
+
+if __name__ == '__main__':
+    main()
