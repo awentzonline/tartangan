81f10507ab51845d8c869552761e2453bab3ca0a
Adam Wentz <adam@vody.com>
2020-04-30 16:34:33 -0700

    Move metrics config to components. Updated README.

--- tartangan/trainers/components/metrics/kubeflow_pipeline.py
+++ tartangan/trainers/components/metrics/kubeflow_pipeline.py
@@ -3,17 +3,14 @@ import os
 
 import smart_open
 
-from tartangan.trainers.components.base import TrainerComponent
 from tartangan.utils.fs import maybe_makedirs
 from tartangan.utils.kubeflow import key_to_kf_name
+from .base import FileBasedMetricsComponent
 
 
-class KubeflowMetricsComponent(TrainerComponent):
+class KubeflowMetricsComponent(FileBasedMetricsComponent):
     """Output metrics in a format suitable for Kubeflow."""
-
-    def __init__(self, output_path, whitelist=None):
-        self.output_path = output_path
-        self.whitelist = whitelist
+    whitelist = None
 
     def on_train_end(self, steps, logs):
         """Outputs the final value for each metric."""
@@ -24,8 +21,8 @@ class KubeflowMetricsComponent(TrainerComponent):
                 if not self.whitelist or key in self.whitelist
             ]
         )
-        dirname = os.path.dirname(self.output_path)
+        dirname = os.path.dirname(self.args.metrics_path)
         if dirname:
             maybe_makedirs(dirname, exist_ok=True)
-        with smart_open.open(self.output_path, 'w') as outfile:
+        with smart_open.open(self.args.metrics_path, 'w') as outfile:
             json.dump(output, outfile)
