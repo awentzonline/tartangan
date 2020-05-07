fd850d4acdc11779ef7cc1f974df7ab998b35b1b
Adam Wentz <adam@vody.com>
2020-05-07 11:06:39 -0700

    Quality updates for explore.find_image

--- tartangan/explore/find_image.py
+++ tartangan/explore/find_image.py
@@ -5,13 +5,11 @@ from PIL import Image
 import smart_open
 import torch
 from torch import nn, optim
-import torchvision
 import torchvision.models as models
 from torchvision import transforms
 import tqdm
 
 from tartangan.utils.fs import maybe_makedirs
-from tartangan.utils.slerp import slerp
 from tartangan.trainers.utils import set_device_from_args
 from .base import GOutputApp
 
@@ -23,9 +21,10 @@ class FindImage(GOutputApp):
     def run(self):
         set_device_from_args(self.args)
         self.load_generator()
-        self.g.requires_grad_(False)
-        self.g.eval()
         self.setup_feature_extractor()
+
+        self.g = self.g.eval()
+        self.g.requires_grad_(False)
 
         if os.path.dirname(self.args.output_prefix):
             maybe_makedirs(os.path.dirname(self.args.output_prefix))
@@ -35,42 +34,59 @@ class FindImage(GOutputApp):
         all_target_feats = []
         with torch.no_grad():
             for feature_extractor in self.feature_extractors:
+                feats = feature_extractor(target_img[None, ...])
                 all_target_feats.append(
-                    feature_extractor(target_img[None, ...])
+                    feats.repeat(self.args.num_samples, 1, 1, 1)
                 )
         # optimize z
-        mse_loss = nn.MSELoss()
+        recon_loss = dict(
+            mse=nn.MSELoss(), l1=nn.SmoothL1Loss()
+        )[self.args.loss]
         z = self.sample_z(self.args.num_samples)
         z.requires_grad_(True)
+        target_imgs = target_img.repeat(self.args.num_samples, 1, 1, 1)
         opt_class = {
             'adam': optim.Adam,
             'sgd': optim.SGD,
+            'lbfgs': optim.LBFGS,
         }[self.args.optimizer]
         optimizer = opt_class([z], self.args.lr)
         tqdm_iter = tqdm.tqdm(range(self.args.max_steps))
-        self.g.train()
-        self.g.requires_grad_(False)
-        for i in tqdm_iter:
+
+        def train_step():
             optimizer.zero_grad()
             imgs = self.g(z)
             vgg_imgs = torch.stack(list(map(self.transform_tanh_to_vgg, imgs)))
-            loss = 0.
-            for feature_extractor, target_feats in zip(
-                    self.feature_extractors, all_target_feats
-            ):
-                img_feats = feature_extractor(vgg_imgs)
-                loss = loss + mse_loss(img_feats, target_feats)
+            if self.args.vgg:
+                loss = 0.
+                for feature_extractor, target_feats in zip(
+                        self.feature_extractors, all_target_feats
+                ):
+                    img_feats = feature_extractor(vgg_imgs)
+                    loss = loss + recon_loss(img_feats, target_feats)
+            else:
+                loss = recon_loss(vgg_imgs, target_imgs)
+            # L2 regularization of latent code
             loss += z.pow(2).mean() * self.args.l2
             loss.backward()
-            optimizer.step()
+            self.save_image(imgs, f'{self.args.output_prefix}_{i}.png')
+            return loss
+
+        for i in tqdm_iter:
+            # stocastic clipping https://openreview.net/pdf?id=HJC88BzFl
+            with torch.no_grad():
+                should_clip = (torch.gt(z, 3) + torch.lt(z, -3)).float()
+                clip_noise = torch.randn_like(z)
+                z -= z * should_clip  # zero out the clipped values
+                z += clip_noise * should_clip
+
+            loss = optimizer.step(train_step)
             z_min, z_mean, z_max = float(z.min()), float(z.mean()), float(z.max())
             tqdm_iter.set_postfix(loss=float(loss), z_min=z_min, z_mean=z_mean, z_max=z_max)
-            self.save_image(imgs, f'{self.args.output_prefix}_{i}.png')
 
     def setup_feature_extractor(self):
-        self.feature_extractor = models.vgg16(pretrained=True).to(self.args.device)
+        self.feature_extractor = models.vgg16(pretrained=True).to(self.args.device).eval()
         self.feature_extractor.requires_grad_(False)
-        #self.feature_extractor.eval()
         self.feature_extractors = []
         for vgg_layer in self.args.vgg_layers:
             feature_extractor = self.feature_extractor.features[:vgg_layer]
@@ -84,11 +100,11 @@ class FindImage(GOutputApp):
                                  std=[0.229, 0.224, 0.225]),
         ])
 
-        def untanh(img):
+        def tanh_inv(img):
             return (img + 1.) / 2.
 
         self.transform_tanh_to_vgg = transforms.Compose([
-            untanh,
+            tanh_inv,
             transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                  std=[0.229, 0.224, 0.225]),
         ])
@@ -98,11 +114,13 @@ class FindImage(GOutputApp):
         super().add_args_to_parser(p)
         p.add_argument('target_image', help='Path to image to be found in G')
         p.add_argument('--max-steps', default=100000, type=int)
-        p.add_argument('--num-samples', default=1, type=int)
-        p.add_argument('--lr', default=0.001, type=float)
+        p.add_argument('--num-samples', default=2, type=int)
+        p.add_argument('--lr', default=0.5, type=float)
+        p.add_argument('--vgg', action='store_true')
         p.add_argument('--vgg-layers', default=(9, 16, 23), type=int, nargs='+')
-        p.add_argument('--optimizer', default='sgd')
-        p.add_argument('--l2', default=0.001, type=float)
+        p.add_argument('--optimizer', default='adam')
+        p.add_argument('--l2', default=0., type=float)
+        p.add_argument('--loss', default='mse')
 
 
 if __name__ == '__main__':
